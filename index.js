db6b9e6e5a7abc61df3173a6878b9d76666f6b8c
DarnishaTurner <darnishaturner@pursuit.org>
2023-10-08 09:27:48 -0400

    My finished lab

--- index.js
+++ index.js
@@ -9,35 +9,46 @@ const newMember = require("./data/new-member");
  * @param {Object} obj -
  * @return {number} The number of keys in the object
  */
-function numberOfKeys(obj) {}
+function numberOfKeys(obj) {
+  return _.keys(obj).length;
+}
 
 /**
  * Remove the falsy values in a numbers array and return the sum
  * @param {numbers[]} array - An array of numbers that can also contain some falsy values
  * @return {number} The sum of the numbers in an array
  */
-function sumNumbers(array) {}
+function sumNumbers(array) {
+  return _.sum(_.remove(array));
+}
 
 /**
  * Convert a two-dimensional array of new member data (each inner array having two values: the first being the key, the second being the value) into an object
  * @param {Array[]} member -
  * @return {number} The sum of the numbers in an array
  */
-function newMemberArrayToObject(member) {}
+function newMemberArrayToObject(member) {
+  return _.fromPairs(member)
+}
 
 /**
  * Return an array of objects that grouped by instructors from the classes array of objects
  * @param {Object[]} collection - an array of yoga class objects
  * @return {Object[]} - the reshaped collection where the classes are grouped by instructor name
  */
-function groupClassByInstructor(collection) {}
+function groupClassByInstructor(collection) {
+  return _.groupBy(collection, "instructor")
+}
 
 /**
  * Remove the age key from the members array of object
  * @param {Object} collection - an array of member objects
  * @return {number} The array of member objects, each one without the age field
  */
-function omitAgeFromMembers(collection) {}
+function omitAgeFromMembers(collection) {
+  return _.map(collection, member => _.omit(member, "age"));
+}
+
 
 /**
  * Return the count of the number of classes a particular instructor teaches
@@ -45,21 +56,43 @@ function omitAgeFromMembers(collection) {}
  * @param {String} instructorName - The name of the instructor
  * @return {number} The sum of the numbers in an array
  */
-function countClassesByInstructor(collection, instructor) {}
+function countClassesByInstructor(collection, instructorName) {
+  const classesByInstructor = _.filter(collection, { instructor: instructorName });
+
+  if (classesByInstructor.length > 0) {
+    return classesByInstructor.length;
+  } else {
+    return "There is no instructor by that name.";
+  }
+}
+ 
 
 /**
  * Remove inactive members from the members array
  * @param {Object} collection - an array of member objects
  * @return {number} The array of member objects with only active members
  */
-function removeInactiveMembers(collection) {}
+function removeInactiveMembers(collection) {
+  return _.filter(collection, "currentMember", true);
+}
 
 /**
  * Get a list of unique class titles and their price
  * @param {Object} collection - an array of yoga class objects
  * @return {number} An array of objects that have a unique title and a price
  */
-function getUniqueClasses(collection) {}
+function getUniqueClasses(collection) {
+  let classes = _.uniqBy(collection, 'title');
+
+  return classes.map((obj) => {
+      return {
+        title: obj.title,
+        priceInCents: obj.priceInCents,
+      };
+    });
+}
+
+
 
 /**
  * Get a list of classes organized by title, then by level.
@@ -67,7 +100,19 @@ function getUniqueClasses(collection) {}
  * @param {Object} collection - an array of yoga class objects
  * @return {number} An array of objects that are organized by title then by level. The array should only have the title, instructor, and level fields
  */
-function orderClassesByTitleAndLevel(collection) {}
+function orderClassesByTitleAndLevel(collection) {
+  const orderedClasses = _.orderBy(collection, ["title", "level"], ["asc", "desc"]);
+
+  const result = orderedClasses.map(classObj => ({
+    title: classObj.title,
+    instructor: classObj.instructor,
+    level: classObj.level,
+  }));
+
+  return result;
+}
+
+// return _.orderBy(collection, ['title', 'level'], ['asc', 'desc']).map(({ title, instructor, level }) => ({ title, instructor, level }));
 
 module.exports = {
   numberOfKeys,
